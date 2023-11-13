b429e7a3b7b2887a10cb293fd7b45ef100aba6e3
XbozzX <afham821@gmail.com>
2023-11-14 00:25:01 +0800

    v0.1.0 - insert the app header

---
+++ components/feed.js
@@ -0,0 +1,27 @@
+import React from 'react'
+
+function feed() {
+  return (
+    <div>
+        {/* Instagram LOGO*/}
+        
+        <div>
+          <p> Instagram </p>
+        </div>
+
+        {/* Search Box*/}
+          <div>
+            <input
+              type='text'
+              placeholder='Search'
+            />
+          </div>
+        {/* Icon */}
+          <div>
+            <p> Icon </p>
+          </div>
+    </div>
+  )
+}
+
+export default feed
